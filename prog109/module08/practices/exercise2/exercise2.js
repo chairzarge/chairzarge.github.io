a3b4a16de6015080a3fd168fef023e3ba9f3cb18
chairzarge <chairzarge@outlook.com>
2023-02-14 16:50:11 -0800

    exercise2

--- prog109/module08/practices/exercise2/exercise2.js
+++ prog109/module08/practices/exercise2/exercise2.js
@@ -1,8 +1,20 @@
 // Select the starting point and find its siblings.
 var startItem = document.getElementById('two');
-var prevItem = startItem.previousSibling;
-var nextItem = startItem.nextSibling;
+var prevItem = startItem.previousElementSibling;
+var nextItem = startItem.nextElementSibling;
 
 // Change the values of the siblings' class attributes.
 prevItem.className = 'complete';
+prevItem.innerHTML += `<img src="imgs/icon-trash.png">`
 nextItem.className = 'cool';
+
+
+
+
+let imgs = document.getElementsByTagName("img");
+
+for (let i = 0; i < imgs.length; i++) {
+    imgs[i].addEventListener("click", e => {
+        imgs[i].parentElement.remove();
+    });
+}
