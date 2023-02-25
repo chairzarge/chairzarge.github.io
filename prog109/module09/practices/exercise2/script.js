af222ce2a67befb32d7164d66724e5b94c1b299e
chairzarge <chairzarge@outlook.com>
2023-02-24 20:09:39 -0800

    exercise 2, iSpy

--- prog109/module09/practices/exercise2/script.js
+++ prog109/module09/practices/exercise2/script.js
@@ -1,6 +1,31 @@
 
 
-let variable = document.createElement();
+let areas = document.getElementsByTagName("area");
+
+let listItems = document.getElementsByTagName("li");
 
 
-variable.removeEvent
+for (let i = 0; i < areas.length; i++) {
+    areas[i].addEventListener("click", clickedArea);
+}
+
+
+
+function clickedArea(event) {
+    event.target.classList.add("clicked");
+    for (let i = 0; i < listItems.length; i++) {
+        if (listItems[i].getAttribute("name") == event.target.getAttribute("name")) {
+            listItems[i].classList.add("crossedOut");
+        }
+    }
+
+    for (let i = 0; i < listItems.length; i++) {
+        if (!listItems[i].classList.contains("crossedOut")) {
+            return;
+        }
+    }
+
+    if (confirm("You have completed the iSpy! Would you like to replay?")) {
+        location.reload();
+    }
+}
